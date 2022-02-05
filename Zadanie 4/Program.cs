9a7cd0c9e8cf48c3e75368cc6fac04fb8e1298da
Evgeniy <ugin2308@mail.ru>
2022-02-05 15:24:14 +0300

    4th done

--- Zadanie 4/Program.cs
+++ Zadanie 4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Введите число больше 1: ");
+
+string numbers = Console.ReadLine();
+
+int number = int.Parse(numbers);
+
+int numberCh = 2;
+
+while (numberCh <= number)
+{
+    Console.Write($"{numberCh}" + " ");
+   numberCh = numberCh+2;
+}
