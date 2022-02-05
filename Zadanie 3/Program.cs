820c2273e3eec6d95012c84e695dc09813280b93
Evgeniy <ugin2308@mail.ru>
2022-02-05 14:30:25 +0300

    3d done

--- Zadanie 3/Program.cs
+++ Zadanie 3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Выяснить является ли число чётным
+
+Console.WriteLine ("Введите число");
+
+string num1 = Console.ReadLine();
+
+int n1 = int.Parse(num1);
+
+int chetnoe = n1 % 2;
+
+if (chetnoe == 0)
+    Console.WriteLine($"Число {n1} является четным");
+    else
+    Console.WriteLine($"Число {n1} является не четным");
