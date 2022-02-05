35768492c902825dc43b515b22b9dc0cef0d05c6
Evgeniy <ugin2308@mail.ru>
2022-02-05 16:12:41 +0300

    5th done

--- Zadanie 5/Program.cs
+++ Zadanie 5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Показать вторую цифру трёхзначного числа.
+int GetSecondDigit(int number)
+{
+    return number = number % 100 / 10;
+    
+}
+
+Console.Write ("Введите трехзначное натруальное число: ");
+
+string number = Console.ReadLine();
+
+int numberToGet = int.Parse(number);
+
+int secDig = GetSecondDigit(numberToGet);
+
+Console.Write($"Вторая цифла заданного числа = {secDig}");
