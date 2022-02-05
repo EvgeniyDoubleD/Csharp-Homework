e6febfd1ddb0790d3ef225b2143a5365a760cf79
Evgeniy <ugin2308@mail.ru>
2022-02-05 20:20:09 +0300

    Test massive

--- Zadanie 7/Program.cs
+++ Zadanie 7/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 14: Найти третью цифру числа или сообщить, что её нет.
+// Попытаюсь решить через массив, мне кажется такой вариант оптимальнее и универсальнее.
+
+Console.Write ("Введите число: ");
+
+string number = Console.ReadLine();
+
+char[] b = number.ToCharArray();
+
+char numbers = b[2];
+    if (numbers = '\0');
+    Console.WriteLine("Нет третьей цифры");
+
+double getnum = Char.GetNumericValue(numbers);
+int toint = Convert.ToInt32(getnum);
+    
+Console.WriteLine($"Третья цифра: {toint}");
+ 
+
+
