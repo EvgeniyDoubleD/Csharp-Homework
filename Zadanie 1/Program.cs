5fb81b51a9afad33fe10faf94884c2e74d40c10d
Evgeniy <ugin2308@mail.ru>
2022-02-05 13:59:16 +0300

    1st done

--- Zadanie 1/Program.cs
+++ Zadanie 1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Даны два числа. Показать большее и меньшее число
+Console.WriteLine ("Введите первое число");
+
+string num1 = Console.ReadLine();
+
+int n1 = int.Parse(num1);
+
+Console.WriteLine ("Введите второе число");
+
+string num2 = Console.ReadLine();
+
+int n2 = int.Parse(num2);
+
+if (n1>n2)
+    Console.WriteLine("Первое число больше второго");
+    else
+    Console.WriteLine("Второе число больше первого");
+
+Console.WriteLine("Вы молодец");
