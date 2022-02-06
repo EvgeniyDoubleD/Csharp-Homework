d79b495d49f958a3c998d2274e1737ccccbe3746
Evgeniy <ugin2308@mail.ru>
2022-02-06 12:43:37 +0300

    8th done

--- Zadanie 8_День_недели_выходной/Program.cs
+++ Zadanie 8_День_недели_выходной/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
+int numberR = new Random().Next(1,8);
+Console.WriteLine("Сгенерирован номер дня недели: "+ numberR);
+
+int budni = 5;
+
+if (numberR <= budni)
+    Console.WriteLine("День недели не выходной, на работу товарищи!");
+else
+    Console.WriteLine("День недели выходной, отдыхаем!");
