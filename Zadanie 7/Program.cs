807bf75d5de7cfc11502a7474161fa3b3164fa2d
Evgeniy <ugin2308@mail.ru>
2022-02-06 20:56:14 +0300

    lets try

--- Zadanie 7/Program.cs
+++ Zadanie 7/Program.cs
@@ -8,26 +8,80 @@
 // char[] b = number.ToCharArray();
 
 // char numbers = b[2];
-//     if (sizeof= '\0');
-//     Console.WriteLine("Нет третьей цифры");
 
 // double getnum = Char.GetNumericValue(numbers);
 // int toint = Convert.ToInt32(getnum);
-    
+// int dec = -1;
+//  if (toint == dec)
+//     Console.WriteLine("введите число а не текст");
+// else 
 // Console.WriteLine($"Третья цифра: {toint}");
+
  
-Console.Write ("Введите число:");
+// // Console.Write ("Введите число:");
 
-string pars = Console.ReadLine();
+// // string pars = Console.ReadLine();
 
-pars = pars.Substring(2,1);
-//  if (pars == null)
+// // pars = pars.Substring(2,1);
+// // //  if (pars == null)
 
-//  Console.WriteLine("нет третьего числа");
+// // //  Console.WriteLine("нет третьего числа");
 
-int number = int.Parse(pars);
+// // int number = int.Parse(pars);
 
- Console.WriteLine($"Третья цифра числа: {number}");
+// //  Console.WriteLine($"Третья цифра числа: {number}");
 
-//Console.WriteLine(pars);
+//Console.WriteLine(pars)
 
+Console.Write("Введите число: ");
+
+string numberstring = Console.ReadLine();
+
+int numberint = int.Parse(numberstring);
+
+int i = 1;
+
+int c = 0;
+
+int j = numberint;
+
+int f = numberint;
+
+
+while (j > i)
+{
+    {   if (j%100 != 1)
+        j = j / 10;
+        c++;
+        Console.WriteLine($"While j = {j}");
+        Console.WriteLine($"Iterations =  {c}");
+    
+
+    }
+
+}
+
+int b = 0;
+
+while (b < c-2)
+{
+    {
+     f = f / 10;
+
+     Console.WriteLine($"Number = {f}");
+
+     b++;
+
+     Console.WriteLine($"Delenie raz ={b}");
+
+
+
+     }
+
+
+}
+
+int x = f % 10;
+
+    Console.WriteLine($"Третья цифра числа - {x}");
+    
