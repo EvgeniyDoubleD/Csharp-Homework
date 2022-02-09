c5ccb4eda8d922420ec48bc0baff76757363285d
Evgeniy <ugin2308@mail.ru>
2022-02-10 00:44:24 +0300

    final done

--- Zadanie 7/Program.cs
+++ Zadanie 7/Program.cs
@@ -33,43 +33,50 @@
 
 //Console.WriteLine(pars)
 
-int InPut(string msg)
-{
-    Console.Write(msg);
-    string num = Console.ReadLine();
-    return int.Parse(num);
-}
+Console.Write("Введите число: ");
 
-int GetCounts(int number)
+string stringNumb = Console.ReadLine();
+
+int.TryParse(stringNumb, out int sint);
+
+string resint = sint.ToString();
+
+int final = int.Parse(resint);
+if (final > 0)
+    Console.WriteLine($"Преобразуем число...");
+else
+Console.WriteLine("Вы ввели не числовое значение");
+
+int GetCounts(int final)
 {   
     int d = 99;
     int c = 0;
-    while (number > d)
+    while (final > d)
     {
-        number = number/10;
+        final = final/10;
         c++;
     }
     return c-1;
 
 }
 
-int GetThirdNumber(int number, int count)
+int GetThirdNumber(int final, int count)
 {
     int track = 0;
      while (track < count)
         {
-            number = number / 10;
+            final = final / 10;
             track++;
         }
-        return number%10;
+        return final%10;
 }
 
 
-int number = InPut("Введите натуральное целое число: ");
-int counts = GetCounts(number);
-int third = GetThirdNumber(number,counts);
 
-if (number<=99)
+int counts = GetCounts(final);
+int third = GetThirdNumber(final,counts);
+
+if (final<=99)
     Console.WriteLine("В числе меньше трех цифр");
 else
     Console.WriteLine($"Третья цифра числа: {third}");
