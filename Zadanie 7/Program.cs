3908b33e0813e98b786a2d11fb86b2f6f71cc1aa
Evgeniy <ugin2308@mail.ru>
2022-02-10 00:46:19 +0300

    final 2.0

--- Zadanie 7/Program.cs
+++ Zadanie 7/Program.cs
@@ -1,37 +1,4 @@
 ﻿// Задача 14: Найти третью цифру числа или сообщить, что её нет.
-// Попытаюсь решить через массив, мне кажется такой вариант оптимальнее и универсальнее.
-
-// Console.Write ("Введите число: ");
-
-// string number = Console.ReadLine();
-
-// char[] b = number.ToCharArray();
-
-// char numbers = b[2];
-
-// double getnum = Char.GetNumericValue(numbers);
-// int toint = Convert.ToInt32(getnum);
-// int dec = -1;
-//  if (toint == dec)
-//     Console.WriteLine("введите число а не текст");
-// else 
-// Console.WriteLine($"Третья цифра: {toint}");
-
- 
-// // Console.Write ("Введите число:");
-
-// // string pars = Console.ReadLine();
-
-// // pars = pars.Substring(2,1);
-// // //  if (pars == null)
-
-// // //  Console.WriteLine("нет третьего числа");
-
-// // int number = int.Parse(pars);
-
-// //  Console.WriteLine($"Третья цифра числа: {number}");
-
-//Console.WriteLine(pars)
 
 Console.Write("Введите число: ");
 
