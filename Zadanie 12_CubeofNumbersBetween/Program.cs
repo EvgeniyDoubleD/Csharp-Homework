f3d29a0dbe04edf5e99be59f99c37cddbe8741c7
Evgeniy <ugin2308@mail.ru>
2022-02-11 00:07:49 +0300

    Comments added+formating

--- Zadanie 12_CubeofNumbersBetween/Program.cs
+++ Zadanie 12_CubeofNumbersBetween/Program.cs
@@ -5,33 +5,31 @@
 // 3. Заполняем массив кубами 
 // 4. Печатаем массив.
 
-int GetRandomNumber(string message)
+int GetRandomNumber(string message)  // Генерируем случайное число
 {
     int numberR = new Random().Next(1,26);
     Console.WriteLine($"Сгенерировано число: {numberR}");
     return numberR;
 }
 
-int [] InitArray(int number)
+int [] InitArray(int number) // Создаем массив
 {
     int [] Array = new int[number];
     return Array;
 }
 
-int [] FillArray(int [] array)
+int [] FillArray(int [] array) // Заполняем массив кубами N
 {
     for ( int i = 0; i < array.Length; i++)
         array[i] = (int)Math.Pow((i+1),3);
         return array;
-
 }
 
-void PrintArray(int [] array)
+void PrintArray(int [] array) // Выводим массив N^ = 
 {
     Console.WriteLine("Число Значение");
     for ( int i = 0; i != array.Length; i++)
         Console.WriteLine($"{i+1}^3 = {array[i]}");
-
 }
 
 int rNumber = GetRandomNumber("RandomN Number Gen");
