6f7e5313fb003891c9a413bee95207c934873000
Evgeniy <ugin2308@mail.ru>
2022-02-11 00:05:14 +0300

    12th done

--- Zadanie 12_CubeofNumbersBetween/Program.cs
+++ Zadanie 12_CubeofNumbersBetween/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Найти кубы чисел от 1 до N , 
+// число N будет рандомное от 1 до 25.
+// 1. Генерируем случайное число
+// 2. Создаем массив.
+// 3. Заполняем массив кубами 
+// 4. Печатаем массив.
+
+int GetRandomNumber(string message)
+{
+    int numberR = new Random().Next(1,26);
+    Console.WriteLine($"Сгенерировано число: {numberR}");
+    return numberR;
+}
+
+int [] InitArray(int number)
+{
+    int [] Array = new int[number];
+    return Array;
+}
+
+int [] FillArray(int [] array)
+{
+    for ( int i = 0; i < array.Length; i++)
+        array[i] = (int)Math.Pow((i+1),3);
+        return array;
+
+}
+
+void PrintArray(int [] array)
+{
+    Console.WriteLine("Число Значение");
+    for ( int i = 0; i != array.Length; i++)
+        Console.WriteLine($"{i+1}^3 = {array[i]}");
+
+}
+
+int rNumber = GetRandomNumber("RandomN Number Gen");
+
+int [] array = InitArray(rNumber);
+
+int [] fArray = FillArray(array);
+
+PrintArray(fArray);
