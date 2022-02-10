360297179cf75c0a994258777f2024b2e375b06c
Evgeniy <ugin2308@mail.ru>
2022-02-10 21:06:13 +0300

    added 0< number checkout

--- Zadanie 7/Program.cs
+++ Zadanie 7/Program.cs
@@ -1,49 +1,52 @@
 ﻿// Задача 14: Найти третью цифру числа или сообщить, что её нет.
 
+// Реализованы следующие проверки: 1. Ввод букв вместо число (сообщит и сбросит). 2. Ввод двузначного числа (сообщит и сбросит) 3. Ввод отрицательного числа (выдаст 3ю цифру.)
+
 Console.Write("Введите число: ");
 
 string stringNumb = Console.ReadLine();
 
-int.TryParse(stringNumb, out int sint);
+int.TryParse(stringNumb, out int sint); // 
 
-string resint = sint.ToString();
+string resint = sint.ToString();  // отбрасываем 0 если пользователь ввел 000123
 
-int final = int.Parse(resint);
-if (final > 0)
+int final = int.Parse(resint); // возвращаем в int 
+int digit = Math.Abs(final); // преобразуем отрицательное число если пользователь ввел -123
+if (digit > 0)
     Console.WriteLine($"Преобразуем число...");
 else
 Console.WriteLine("Вы ввели не числовое значение");
 
-int GetCounts(int final)
+int GetCounts(int digit) /// считаем разряды числа 
 {   
     int d = 99;
     int c = 0;
-    while (final > d)
+    while (digit > d)
     {
-        final = final/10;
+        digit = digit/10;
         c++;
     }
     return c-1;
 
 }
 
-int GetThirdNumber(int final, int count)
+int GetThirdNumber(int digit, int count) // Получаем третью цифру числа, сокращаем его до 3 потом остаток от деления
 {
     int track = 0;
      while (track < count)
         {
-            final = final / 10;
+            digit = digit / 10;
             track++;
         }
-        return final%10;
+        return digit%10;
 }
 
 
 
-int counts = GetCounts(final);
-int third = GetThirdNumber(final,counts);
+int counts = GetCounts(digit); /// получаем разряды 
+int third = GetThirdNumber(digit,counts); // сокращаем и получаем 3 цифру числа
 
-if (final<=99)
+if (digit<=99)
     Console.WriteLine("В числе меньше трех цифр");
 else
     Console.WriteLine($"Третья цифра числа: {third}");
