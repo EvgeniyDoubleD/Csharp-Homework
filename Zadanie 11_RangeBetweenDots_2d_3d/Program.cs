b9b712a25eb38ac4cb0e4337a180024bfae4a371
Evgeniy <ugin2308@mail.ru>
2022-02-12 03:43:32 +0300

    Dots done

--- Zadanie 11_RangeBetweenDots_2d_3d/Program.cs
+++ Zadanie 11_RangeBetweenDots_2d_3d/Program.cs
@@ -1,38 +1,45 @@
 ﻿// Задача 22: Найти расстояние между точками в пространстве 2D/3D
-// Пользователь вводит поочередно координаты х - у - z - для каждой точки. Тип пусть будет дабл.
+// Пользователь вводит поочередно координаты х - у - z - для двух точек А и В (опционально можно расширить пулл точек) Тип пусть будет дабл.
 // Координаты проверяются на ввод текста /repeat , 0123 - дабл решает проблему ,  отрицательные значения допустимы.
 // Расчёт расстояния между точек: на выбор  0 - 2д 1 - 3д. Если пользователь задал (1.2.0)
 
-double GetCoordinate(string msg) // метод ввода точки координат с проверкой на текст введенный.
+double GetCoordinate(string msg) // метод ввода точки координат с проверкой "на дурака"
 {
-    double result = 0;
+    double coordinate = 0;
+    bool result = false;
     string message = null;
-    while (result == 0 || message is null)
+    while (result == false || message is null)
     {
         Console.Write(msg);
         string messageIn = Console.ReadLine();
-        double.TryParse(message, out double result_check);
-        result = result_check;
-        message = messageIn;
+        if (result = double.TryParse(messageIn, out double result_check));
+           {
+            coordinate = result_check;
+            message = messageIn;
+           }
+        
     }
-    return result;
+    return coordinate;
 }   
+void DoNiceFormatOutput() // метод "красивого" разделения вывода данных в консоли :>
+{
+    Console.WriteLine("======================================");
+}
 
-double x_one = GetCoordinate("Параметр точки №1. Введите координаты по оси Х: ");
-double y_one = GetCoordinate("Параметр точки №1. Введите координаты по оси Y: ");
-double z_one = GetCoordinate("Параметр точки №1. Введите координаты по оси Z: ");
-double x_two = GetCoordinate("Параметр точки №2. Введите координаты по оси Х: ");
-double y_two = GetCoordinate("Параметр точки №2. Введите координаты по оси Y: ");
-double z_two = GetCoordinate("Параметр точки №2. Введите координаты по оси Z: ");
+DoNiceFormatOutput();
 
-Console.WriteLine($"Координаты точки №1: {x_one} {y_one} {z_one}\nКоординаты точки №2: {x_two} {y_two} {z_two}");
+double x_one = GetCoordinate("Точка A.\nВведите координаты по оси Х: ");
+double y_one = GetCoordinate("Введите координаты по оси Y: ");
+double z_one = GetCoordinate("Введите координаты по оси Z: ");
+double x_two = GetCoordinate("Точка B.\nВведите координаты по оси Х: ");
+double y_two = GetCoordinate("Введите координаты по оси Y: ");
+double z_two = GetCoordinate("Введите координаты по оси Z: ");
 
-double distance = Math.Sqrt((Math.Pow((x_two - x_one),2) + Math.Pow((y_two - y_one),2) + Math.Pow((z_two - z_one),2))); // формула расчета расстояния 3д работает 2д под вопросом.
+DoNiceFormatOutput();
+Console.WriteLine($"Координаты точки А: ({x_one},{y_one},{z_one})\nКоординаты точки B: ({x_two},{y_two},{z_two})");
+DoNiceFormatOutput();
 
-Console.WriteLine($"Расстояние между указаными точками : {distance}");
+double distance = Math.Sqrt((Math.Pow((x_two - x_one),2) + Math.Pow((y_two - y_one),2) + Math.Pow((z_two - z_one),2))); // формула расчета расстояния 2д/3д смотря как заданы координаты.
 
-double checkout = Math.Sqrt((Math.Pow((x_two - (x_one)),2)));
-
-double check = Math.Sqrt((Math.Pow((y_two - y_one),2)));
-
-Console.WriteLine($"checkout = {checkout}; check = {check}");
+Console.WriteLine($"Расстояние между указанными точками составляет : {distance}");
+DoNiceFormatOutput();
