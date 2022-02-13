72edf1dfd30410ee4d876196f59eb6c624ca1ba9
Evgeniy <ugin2308@mail.ru>
2022-02-14 00:50:44 +0300

    done but not perfect. wanna remove 10/20/30

--- Zadanie 15_Print%2_cubes/Program.cs
+++ Zadanie 15_Print%2_cubes/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+// формируем "случайный" массив из чисел которые заканчиваются на четную цифру
+// возводим в кубы 
+// выводим на печать?
+
+int numberR = new Random().Next(1,35);
+Console.WriteLine($"Сгенерировано число: {numberR}");
+int [] array = new int[numberR];
+int [] FillArray(int [] array) // Заполняем массив кубами N
+{
+    for ( int i = 0; i < array.Length; i++)
+        if ((i+1)%2 == 0)
+            array[i] = (int)Math.Pow((i+1),3);
+            return array;
+}
+
+void PrintArray(int [] array) // Выводим массив N^ = 
+{
+    Console.WriteLine("Число Значение");
+    for ( int i = 0; i != array.Length; i++)
+        if (array[i] != 0)
+        Console.WriteLine($"{i+1}^3 = {array[i]}");
+}
+
+int [] arrays = FillArray(array);
+
+PrintArray(arrays);
