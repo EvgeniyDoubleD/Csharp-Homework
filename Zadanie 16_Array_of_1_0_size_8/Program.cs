e77455f851fb3f16cf1e241387f4e9079ed1c860
Evgeniy <ugin2308@mail.ru>
2022-02-15 00:22:41 +0300

    done i think. not hard one :(

--- Zadanie 16_Array_of_1_0_size_8/Program.cs
+++ Zadanie 16_Array_of_1_0_size_8/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 32: Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+// 1. Создаем массив на 8 элементов, сразу, поскольку в задаче указано :) 
+// 2. Заполняем массив через рандом, в целом задача не оч сложная..
+
+void FillArray(int [] array)
+{
+    Random rNum = new Random();
+    for ( int i = 0; i != array.Length; i++)
+        array [i] = rNum.Next(0,2);
+        
+
+}
+void PrintArray(int [] array) // Выводим массив N^ = 
+{
+    for ( int i = 0; i != array.Length; i++)
+        Console.Write($"[ {array[i]} ]");
+}
+
+int [] array = new int[8];
+FillArray(array);
+PrintArray(array);
