b2b31b9c9cfab76f49fea2d94eccec231f89222f
Evgeniy <ugin2308@mail.ru>
2022-02-16 00:22:52 +0300

    final

--- Zadanie 15_Print%2_cubes/Program.cs
+++ Zadanie 15_Print%2_cubes/Program.cs
@@ -1,5 +1,5 @@
 ﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
-// формируем "случайный" массив из чисел которые заканчиваются на четную цифру
+// формируем "случайный" массив 
 // возводим в кубы 
 // выводим на печать?
 
@@ -7,20 +7,22 @@ int numberR = new Random().Next(1,55);
 Console.WriteLine($"Сгенерировано число: {numberR}");
 
 int [] array = new int[numberR];
-int [] FillArray(int [] array) // Заполняем массив кубами N
+int [] FillArray(int [] array) // Заполняем массив (учтено замечание к пред. дз и просто заполнил массив)
 {
     for ( int i = 0; i < array.Length; i++)
-        if ((i+1)%2 == 0)
-            array[i] = (int)Math.Pow((i+1),3);
+            array[i] = i+1;
             return array;
 }
 
-void PrintArray(int [] array) // Выводим массив N^ = 
+void PrintArray(int [] array) // Выводим массив N^3 = array[i]
 {
     Console.WriteLine("Число Значение");
     for ( int i = 0; i != array.Length; i++)
-        if (array[i] != 0)
+        if (array[i] != 0 & (i+1)%2 == 0)
+        {
+        array[i] = (int)Math.Pow((i+1),3);
         Console.WriteLine($"{i+1}^3 = {array[i]}");
+        }
 }
 
 int [] arrays = FillArray(array);
