ff210a99eb441ad2351df2040e20be7379b07345
Evgeniy <ugin2308@mail.ru>
2022-02-19 14:42:14 +0300

    final v 1.00

--- Zadanie 19_Summ_of_array_odd_index/Program.cs
+++ Zadanie 19_Summ_of_array_odd_index/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See Задача 38: Найти сумму чисел одномерного массива стоящих на нечетной позиции.
+// задаем массив N размера
+// заполняем его рандом значениями
+// создаем переменную накопитель, в которую складываем сумму элементов стоящих на нечтеном индексе выводим
+
+void FillAndPrintArray(int [] array) // заполняем массив рандом значениями и сразу же его выводим в терминал;
+{
+    Random rNum = new Random();
+    for ( int i = 0; i != array.Length; i++)
+        {
+        array [i] = rNum.Next(1,15);
+        Console.Write($"[{array[i]}]\t");
+        }
+}
+
+void SummOfOddIndexElems(int [] array) // считаем сумму эл-тов на нечетной позиции
+{
+    int summOdd = 0;
+    for (int i = 0; i != array.Length; i++)
+        {
+            if ((i+1)%2 != 0)
+                summOdd += array[i];
+        }
+    Console.WriteLine($"Cумма чисел стоящих на не четной позиции массива: {summOdd}");
+}
+
+void FineBreakOnConsole(string msg) // красиво разделяем вывод в терминал
+{
+    Console.WriteLine(msg);
+}
+int [] array = new int [9];
+
+FineBreakOnConsole("Сгенерирован массив:");
+FillAndPrintArray(array);
+FineBreakOnConsole("");
+SummOfOddIndexElems(array);
