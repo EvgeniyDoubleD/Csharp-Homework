7dc4ad70a954994a22e53656af803ae064f18a96
Evgeniy <ugin2308@mail.ru>
2022-02-20 22:54:46 +0300

    done?

--- Zadanie 22_Tochka_peresechenia/Program.cs
+++ Zadanie 22_Tochka_peresechenia/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2    b1 k1 и b2 и k2 заданы
+// сперва надо решить систему уравнений  y-y = k1*x+b1-k2*x+b2; 0 = k1*x-b1 y = k2*x + b2; 
+// x = b1/k1 y = k2*x+b2
+
+double b1 = 2;
+double k1 = 5;
+double b2 = 1;
+double k2 = -3;
+
+double x = b1/k1;
+double y = k2*x+b2;
+
+Console.WriteLine($"Точка пересечения: ({x:N3} , {y:N3} )");
