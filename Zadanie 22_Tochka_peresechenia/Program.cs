bda52984ea2fbc672102519af0133e1f257e3a19
Evgeniy <ugin2308@mail.ru>
2022-02-23 13:09:19 +0300

    donev1.0

--- Zadanie 22_Tochka_peresechenia/Program.cs
+++ Zadanie 22_Tochka_peresechenia/Program.cs
@@ -1,13 +1,17 @@
 ﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2    b1 k1 и b2 и k2 заданы
-// сперва надо решить систему уравнений  y-y = k1*x+b1-k2*x+b2; 0 = k1*x-b1 y = k2*x + b2; 
+// сперва надо решить систему уравнений  приравняем уравнения:
+// K1x+b1=K2x+b2 -> K1x-K2x=b2-b1 -> x = (b2-b1)/(K1-K2)
 // x = b1/k1 y = k2*x+b2
 
-double b1 = 2;
-double k1 = 5;
+double b1 = -1;
+double k1 = 2;
 double b2 = 1;
-double k2 = -3;
+double k2 = 2;
 
-double x = b1/k1;
-double y = k2*x+b2;
+double x = (b2-b1)/(k1-k2);
+double y = (k2*x)+b2;
 
+if (k1 == k2)
+    Console.WriteLine("Прямые не пересекаются");
+else
 Console.WriteLine($"Точка пересечения: ({x:N3} , {y:N3} )");
