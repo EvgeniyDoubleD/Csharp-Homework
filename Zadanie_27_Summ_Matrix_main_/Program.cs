ff4fdd0600a5e73b71d5d65d516b3a08e0b4f305
Evgeniy <ugin2308@mail.ru>
2022-02-26 17:51:15 +0300

    doner?

--- Zadanie_27_Summ_Matrix_main_/Program.cs
+++ Zadanie_27_Summ_Matrix_main_/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: В матрице чисел найти сумму элементов главной диагонали.
+int[,] arr = new int[4, 4];
+
+void MainMatrixSumm(int[,] array) // 
+{
+    int c = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = 0;
+
+            if (i == j) // || (i+j == 0)
+            {
+                array[i,j] = 1;
+                c += array[i, j];
+            }
+
+            Console.Write($"[{array[i, j]}]\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine($"Сумма главной диагонали  = {c}");
+}
+
+
+
+MainMatrixSumm(arr);
