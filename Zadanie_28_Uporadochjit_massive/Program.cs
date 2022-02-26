d4d7f1814339a3917bc92c897614ba12f236227f
Evgeniy <ugin2308@mail.ru>
2022-02-26 23:46:49 +0300

    neo4en

--- Zadanie_28_Uporadochjit_massive/Program.cs
+++ Zadanie_28_Uporadochjit_massive/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See Задача 57: Написать программу, которая упорядочивает по убыванию элементы каждой строки двумерной массива.
+
+int[,] arr = new int[4, 4];
+
+void MainMatrixSumm(int[,] array) // 
+{
+    int c = 1;
+    int s = array.GetLength(0);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (i == s)
+            c = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i+j+c;
+            Console.Write($"[{array[i, j]}]\t");
+            c++;
+            if (array[i,j] == (array[0,j]))
+                {
+                    array[i, j] = i-1+c;
+                }
+        }
+        Console.WriteLine();
+    }
+
+
+}
+
+
+
+MainMatrixSumm(arr);
