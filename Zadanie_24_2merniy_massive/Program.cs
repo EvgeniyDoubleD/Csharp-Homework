7a67f94f829122289628e312e1700a873a819b90
Evgeniy <ugin2308@mail.ru>
2022-02-26 16:32:36 +0300

    done?

--- Zadanie_24_2merniy_massive/Program.cs
+++ Zadanie_24_2merniy_massive/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 48: Показать двумерный массив размером m×n заполненный целыми числами.
+
+int [,] arr = new int[5,5];
+
+void FillAndPrintArray(int[,] array) // заполняем массив рандом 1.2 и сразу же его печатаем.
+{
+    Random rNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rNum.Next(0, 2);
+            Console.Write($"[ {array[i, j]} ]");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillAndPrintArray(arr);
