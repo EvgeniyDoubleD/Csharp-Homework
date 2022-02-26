a9e22bbc774f644505adf0642f82d8eb230bf8cd
Evgeniy <ugin2308@mail.ru>
2022-02-26 16:41:08 +0300

    perfect

--- Zadanie_25_Chetnie_na_minus/Program.cs
+++ Zadanie_25_Chetnie_na_minus/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 50: В двумерном массиве n×k заменить четные элементы на противоположные.
+
+int[,] arr = new int[5, 5];
+
+void FillAndPrintMinusOddArray(int[,] array) // заполняем массив рандом 1.2 и сразу же его печатаем.
+{
+    Random rNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rNum.Next(10, 111);
+            
+            int k = array[i, j];
+
+            if (k % 2 == 0)
+            {
+                array[i, j] *= -1;
+            }
+
+            Console.Write($"[{array[i, j]}]\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillAndPrintMinusOddArray(arr);
+
