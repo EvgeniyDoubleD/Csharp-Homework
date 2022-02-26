baa95bc01632d8a925c1a97d233e1a64e29cc61c
Evgeniy <ugin2308@mail.ru>
2022-02-26 18:07:46 +0300

    final v.1.0.3

--- Zadanie_25_Chetnie_na_minus/Program.cs
+++ Zadanie_25_Chetnie_na_minus/Program.cs
@@ -9,7 +9,7 @@ void FillAndPrintMinusEvenArray(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = rNum.Next(3,5);
+            array[i, j] = rNum.Next(3, 5);
 
             int k = array[i, j];
 
