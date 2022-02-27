f0c22908261ab161454d77f2bcab42d4da0f7f4a
Evgeniy <ugin2308@mail.ru>
2022-02-27 18:31:55 +0300

    rovnoe zapolnenie

--- Zadanie_28_Uporadochjit_massive/Program.cs
+++ Zadanie_28_Uporadochjit_massive/Program.cs
@@ -8,17 +8,11 @@ void MainMatrixSumm(int[,] array) //
     int s = array.GetLength(0);
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        if (i == s)
-            c = 0;
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = i+j+c;
+            array[i, j] = c;
             Console.Write($"[{array[i, j]}]\t");
             c++;
-            if (array[i,j] == (array[0,j]))
-                {
-                    array[i, j] = i-1+c;
-                }
         }
         Console.WriteLine();
     }
