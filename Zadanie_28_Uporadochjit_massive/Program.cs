18d096cc57535f6e73b3f08388b7b2fee1f0664f
Evgeniy <ugin2308@mail.ru>
2022-03-02 21:08:25 +0300

    22

--- Zadanie_28_Uporadochjit_massive/Program.cs
+++ Zadanie_28_Uporadochjit_massive/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See Задача 57: Написать программу, которая упорядочивает по убыванию элементы каждой строки двумерной массива.
 
-int[,] arr = new int[4, 4];
+int[,] arr = new int[5, 5];
 
 void MainMatrixSumm(int[,] array) // 
 {
@@ -17,9 +17,49 @@ void MainMatrixSumm(int[,] array) //
         Console.WriteLine();
     }
 
+}
 
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            int maxNumb = array[i, j];
+            if (maxNumb < array[i, j + 1])
+            {
+                maxNumb = array[i, j + 1];
+                array[i, j + 1] = array[i, j];
+                array[i, j] = maxNumb;
+                // Console.WriteLine("NACHALO CIKLA");
+                // Console.WriteLine("-----------");
+                // Console.WriteLine($" stroka = {i} ; stolbec =  {j} ; maxnumb = {maxNumb} transfer - {array[i, j + 1]} <--> {array[i, j]}");
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        SortArray(arr);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"[{array[i, j]}]\t");
+        }
+        Console.WriteLine();
+    }
 }
 
 
-
 MainMatrixSumm(arr);
+Console.WriteLine("========");
+PrintArray(arr);
+
+
+
+// array[i, j] = c;
+// Console.Write($"[{array[i, j]}]\t");
+// c++;
