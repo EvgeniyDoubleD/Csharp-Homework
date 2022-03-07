997c3455abfbbf0e1927bf01120da2a16f892447
Evgeniy <ugin2308@mail.ru>
2022-03-07 20:09:38 +0300

    tufta

--- Zadanie_30_Proizvedenie_matrix/Program.cs
+++ Zadanie_30_Proizvedenie_matrix/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 61. Найти произведение двух матриц
+// Произведением двух матриц будет матрица , элементы которой равны сумме попарных произведений элементов
+// строки первой матрицы на соответствующие элементы столбца второй матрицы.
+//Из этого следует что перемножить между собой можно матрицы в которых количество столбцов первой равно количеству строк второй
+//овая матрица которая является произведением двух имеет размерность , где – количество строк первой матрицы, а – столбцов второй. 
+//Правила достаточно просты и для нахождения произведения матриц нужно уметь лиш умножать и прибавлять
+
+
+int[,] matrixOne = new int[2, 2];
+int[,] matrixTwo = new int[2, 2];
+
+void FillAndPrintRArray(int[,] array) // 
+{
+    Random randomNum = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = randomNum.Next(2, 4);
+            Console.Write($"[{array[i, j]}]\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultiMaxtix(int[,] array1, int[,] array2)
+{
+
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            int result = 0;
+            int result2 = 0;
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                result = array1[i, k] * array2[k, j];
+                result2 += result;
+            }
+            Console.Write($"[{result}]\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillAndPrintRArray(matrixOne);
+Console.WriteLine("=======");
+FillAndPrintRArray(matrixTwo);
+Console.WriteLine("=======");
+Console.WriteLine("resultmatrix:");
+MultiMaxtix(matrixOne, matrixTwo);
