ee508510b0e160167cdcca4fd5ee108123395fa4
Evgeniy <ugin2308@mail.ru>
2022-03-09 14:09:08 +0300

    done v 1.0.0

--- Zadanie_31_3d_massive/Program.cs
+++ Zadanie_31_3d_massive/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63: Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+//
+
+int[,,] threedmassive = new int[3, 3, 3];
+
+void FillAndPrintRArray(int[,,] array) // 
+{
+    int mNum = 10;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = mNum;
+                Console.Write($"Index: [{i},{j},{k}] Value: [{array[i, j, k]}]\t\n");
+                mNum++;
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+FillAndPrintRArray(threedmassive);
