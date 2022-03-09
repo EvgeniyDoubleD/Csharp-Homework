6ce23a29f0425e858db39673abda837c9cfcd9d7
Evgeniy <ugin2308@mail.ru>
2022-03-09 18:01:17 +0300

    done v.1.0.1

--- Zadanie_32_Spiralno_zapoln_massive/Program.cs
+++ Zadanie_32_Spiralno_zapoln_massive/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 65: Спирально заполнить двумерный массив
+
+
+int[,] matrixTwo = new int[5, 5];
+
+void FillArray(int[,] array) // 
+{
+    int maxRows = array.GetLength(0) - 1; //+
+    int maxColumn = array.GetLength(1) - 1; //+
+    int numb = 1; //++
+    int maxNumb = array.GetLength(0) * array.GetLength(1); //+
+    int currentRow = 0; // +
+    int currentColumn = 0; //+ 
+
+    while (numb != maxNumb)
+    {
+        for (int i = currentColumn; i <= maxColumn; i++)
+        {
+            array[currentRow, i] = numb;
+            numb++;
+        }
+        currentRow++;
+        for (int i = currentRow; i <= maxRows; i++)
+        {
+            array[i, maxColumn] = numb;
+            numb++;
+        }
+        maxColumn--;
+        for (int i = maxColumn; i >= currentColumn; i--)
+        {
+            array[maxRows, i] = numb;
+            numb++;
+        }
+        maxRows--;
+        for (int i = maxRows; i >= currentRow; i--)
+        {
+            array[i, currentColumn] = numb;
+            numb++;
+        }
+        currentColumn++;
+
+        if (currentColumn == currentRow)
+            array[currentRow, currentColumn] = numb;
+    }
+}
+
+void PrintRArray(int[,] array) // 
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"[{array[i, j]}]\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(matrixTwo);
+PrintRArray(matrixTwo);
