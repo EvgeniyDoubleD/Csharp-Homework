962b8edf338f452ea89a10e35304dadf7bb31186
Evgeniy <ugin2308@mail.ru>
2022-03-14 21:57:25 +0300

    done v 1.0.0

--- Zadanie_35_Ackerman_recursion/Program.cs
+++ Zadanie_35_Ackerman_recursion/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+
+int GetAnaturalNumber(string msg) // метод ввода числа с проверкой "на дурака"
+{
+    int number = 0;
+    bool result = false;
+    string message = null;
+    while (result == false || message is null)
+    {
+        Console.Write(msg);
+        string messageIn = Console.ReadLine();
+        if (result = int.TryParse(messageIn, out int result_check) && result_check >= 1) ;
+        {
+            number = result_check;
+            message = messageIn;
+        }
+    }
+    return number;
+}
+
+int AckermanTry(int numM, int numN)
+{
+    if (numM == 0)
+        return numN + 1;
+    else if (numN == 0)
+        return AckermanTry(numM - 1, 1);
+    else
+        return AckermanTry(numM - 1, AckermanTry(numM, numN - 1));
+
+}
+
+int m = GetAnaturalNumber("Введите натуральное число M: ");
+int n = GetAnaturalNumber("Введите натуральное число N: ");
+
+int ackerman = AckermanTry(m, n);
+
+Console.WriteLine($"Ackerman function = {ackerman}");
